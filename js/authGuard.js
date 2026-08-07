0b15268e99a8b1ad76b644455305664b351cb12b
hilminajibac-afk <hilminajib.ac@gmail.com>
2026-08-07 14:27:19 +0700

    Add files via upload

---
+++ js/authGuard.js
@@ -0,0 +1,11 @@
+(async () => {
+
+    const { data } = await supabaseClient.auth.getSession();
+
+    if (data.session) {
+
+        window.location.href = "index.html";
+
+    }
+
+})();
---
+++ js/supabase.js
@@ -0,0 +1,8 @@
+const SUPABASE_URL = "https://yypialceunsukaypwsol.supabase.co";
+
+const SUPABASE_KEY = "sb_publishable_FKQaAyVmfiUPBuE000XISw_q6fEz9Mm";
+
+const supabaseClient = supabase.createClient(
+    SUPABASE_URL,
+    SUPABASE_KEY
+);
