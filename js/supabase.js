54f02fe4b6795c25a6b02c90ddd3307127d905bf
hilminajibac-afk <hilminajib.ac@gmail.com>
2026-08-07 14:22:16 +0700

    Delete js directory

--- js/supabase.js
+++
@@ -1,8 +0,0 @@
-const SUPABASE_URL = "https://yypialceunsukaypwsol.supabase.co";
-
-const SUPABASE_KEY = "sb_publishable_FKQaAyVmfiUPBuE000XISw_q6fEz9Mm";
-
-const supabaseClient = supabase.createClient(
-    SUPABASE_URL,
-    SUPABASE_KEY
-);
